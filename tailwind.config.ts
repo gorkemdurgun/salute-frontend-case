8e096d52066d78c2f41c8861ca72249531d18629
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-14 15:00:10 +0300

    Refactor Tailwind CSS colors configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,39 +6,29 @@ export default {
     extend: {
       colors: {
         primary: {
-          blue: "#0052CC", // B400 - Pacific bridge
-          white: "#FFFFFF", // N0 - Doctor
-          neutral: "#172B4D", // N800 - Squid ink
+          light: "#4C9AFF", // Açık Mavi
+          DEFAULT: "#0052CC", // Jira Ana Rengi (Mavi)
+          dark: "#172B4D", // Daha Koyu Mavi
         },
-        secondary: {
-          red: "#FF5630", // R300 - Poppy surprise
-          yellow: "#FFAB00", // Y300 - Golden state
-          green: "#36B37E", // G300 - Fine pine
-          teal: "#00B8D9", // T300 - Tamarama
-          purple: "#6554C0", // P300 - Da' juice
+        success: {
+          light: "#79F2C0", // Açık Yeşil
+          DEFAULT: "#36B37E", // Başarı Yeşili
+          dark: "#006644", // Koyu Yeşil
         },
-        extended: {
-          slate: "#091E42", // N900
-          concrete: "#505F79", // N400
-          bling: "#6B778C", // N200
-          karl: "#C1C7D0", // N50
-          meredith: "#8993A4", // N90
+        warning: {
+          light: "#FFAB00", // Açık Turuncu
+          DEFAULT: "#FF991F", // Uyarı Turuncusu
+          dark: "#BF7317", // Koyu Turuncu
         },
-        red: {
-          500: "#BF2600", // R500 - Dragon's blood
-          400: "#DE350B", // R400 - Red dirt
-          300: "#FF5630", // R300 - Poppy surprise
-          200: "#FF7452", // R200 - Salmon sashimi
-          100: "#FF8F73", // R100 - Alexandria
-          50: "#FFEBE6", // R50 - Rosie
+        danger: {
+          light: "#FF5630", // Açık Kırmızı
+          DEFAULT: "#DE350B", // Hata Kırmızısı
+          dark: "#BF2600", // Koyu Kırmızı
         },
-        blue: {
-          500: "#0747A6", // B500 - Chore coat
-          400: "#0052CC", // B400 - Pacific bridge
-          300: "#0065FF", // B300 - Sodium explosion
-          200: "#2684FF", // B200 - Coogee
-          100: "#4C9AFF", // B100 - Arvo breeze
-          50: "#DEEBFF", // B50 - Pixie dust
+        neutral: {
+          light: "#F4F5F7", // Açık Gri
+          DEFAULT: "#C1C7D0", // Nötr Gri
+          dark: "#42526E", //
         },
       },
     },
