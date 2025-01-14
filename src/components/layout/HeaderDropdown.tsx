3ca30b3095884f6ef658bf25245ca450759f63fe
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-14 15:28:46 +0300

    Enhance HeaderDropdown component with outside click detection

--- src/components/layout/HeaderDropdown.tsx
+++ src/components/layout/HeaderDropdown.tsx
@@ -1,32 +1,51 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { PiCaretDownBold as ArrowDownIcon } from "react-icons/pi";
 
 type Props = HeaderMenuDropdown;
 
 const HeaderDropdown: React.FC<Props> = ({ title, items }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  // Menüyü kapatmak için dış tıklama kontrolü
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   return (
-    <div className="relative px-2">
+    <div ref={dropdownRef} className="relative px-2">
       <button onClick={() => setIsOpen(!isOpen)} className="transition-all flex items-center space-x-1 text-neutral-dark hover:text-neutral">
         <span className="text-sm">{title}</span>
-        <ArrowDownIcon className="w-3 h-3" />
+        <ArrowDownIcon className={`w-3 h-3 transform transition-transform ${isOpen ? "rotate-180" : "rotate-0"}`} />
       </button>
-      {isOpen && (
-        <div className="absolute z-10 w-full mt-2 bg-white rounded-lg shadow-lg">
-          {items.map((item, index) => (
-            <a
-              key={index}
-              href={item.href}
-              className="block px-4 py-2 text-sm text-neutral-dark hover:bg-neutral-light hover:text-neutral-dark"
-            >
-              {item.title}
-            </a>
-          ))}
-        </div>
-      )}
+      <div
+        className={`absolute z-10 w-full min-w-[200px] mt-2 bg-white rounded-lg shadow-2xl overflow-hidden transition-all duration-300 ease-in-out transform ${
+          isOpen ? "opacity-100 scale-100 pointer-events-auto visible" : "opacity-0 scale-95 pointer-events-none invisible"
+        }`}
+        style={{ transformOrigin: "top center" }}
+      >
+        {items.map((item, index) => (
+          <a
+            key={index}
+            href={item.href}
+            className="block w-full px-4 py-2 text-sm text-neutral-dark hover:bg-neutral-light hover:text-neutral-dark text-left border-neutral-light border-b last:border-b-0"
+          >
+            {item.title}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
