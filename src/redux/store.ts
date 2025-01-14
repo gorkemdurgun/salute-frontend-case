6912718a7a6179951bbe7afc865d26b64f172f46
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-14 22:11:12 +0300

    Add Redux Toolkit and Redux Persist for state management, Configurated store.ts

---
+++ src/redux/store.ts
@@ -0,0 +1,34 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import taskReducer from "@/redux/reducers/taskReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["tasks"],
+};
+
+const rootReducer = combineReducers({
+  tasks: taskReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
+});
+
+export const persistor = persistStore(store);
+export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
