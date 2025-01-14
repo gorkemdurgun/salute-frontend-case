f4be66751d0ec63637489e6ec78f624b92236b18
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-14 18:09:03 +0300

    Add ProjectLogo SVG and update SVG index export

--- src/assets/images/svg/index.ts
+++ src/assets/images/svg/index.ts
@@ -1,7 +1,9 @@
 import JiraLogo from "./jira-logo.svg";
+import ProjectLogo from "./project-logo.svg";
 import UserAvatar from "./user-avatar.svg";
 
 export const svg = {
   JiraLogo,
+  ProjectLogo,
   UserAvatar,
 };
