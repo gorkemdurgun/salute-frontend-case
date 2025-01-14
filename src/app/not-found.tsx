576cc0c24353a5d221842a6b07282344d1a70140
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-14 22:43:55 +0300

    Add custom 404 page with Not Found SVG and navigation button

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { svg } from "@/assets/images/svg";
+import Button from "@/components/common/Button";
+import ImageBox from "@/components/common/ImageBox";
+import { useRouter } from "next/navigation";
+
+export default function Custom404() {
+  const router = useRouter();
+  return (
+    <div className="flex items-center justify-center gap-16 h-screen bg-white">
+      <ImageBox src={svg.NotFound} alt="Not Found" className="w-96 h-96 animate-bounce" />
+      <div className="space-y-4 text-center">
+        <h2 className="text-2xl font-medium text-neutral-dark">Böyle bir sayfa yok</h2>
+        <p className="text-neutral-dark/60">Bu kadar derinlere inmeye çalışma, burada bir şey yok.</p>
+        <Button className="!mt-8 px-8 py-2 text-sm font-semibold text-white bg-black" onClick={() => router.push("/jira")}>
+          Jiraya geri dön
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/assets/images/svg/index.ts
+++ src/assets/images/svg/index.ts
@@ -1,9 +1,11 @@
 import JiraLogo from "./jira-logo.svg";
 import ProjectLogo from "./project-logo.svg";
 import UserAvatar from "./user-avatar.svg";
+import NotFound from "./not-found.svg";
 
 export const svg = {
   JiraLogo,
   ProjectLogo,
   UserAvatar,
+  NotFound,
 };
