040b3c99f43023fab7ab4ea27ade9fc199678aff
gorkemdurgun <gorkemdurgunn@gmail.com>
2025-01-16 03:03:23 +0300

    Update dummy datas

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,51 +1,51 @@
 export const users: User[] = [
   {
     id: '3e10b298-788f-472b-87e8-e53468202e1d',
-    name: 'Jon Snow',
-    email: 'jon@stark.com',
+    name: 'Harry Potter',
+    email: 'harry@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440',
+      'https://static.wikia.nocookie.net/harrypotter/images/c/ce/Harry_Potter_DHF1.jpg/revision/latest?cb=20140603201724',
   },
   {
     id: '85bc575d-0c9b-4c30-9202-1e118767c583',
-    name: 'Daenerys Targaryen',
-    email: 'daenerys@targaryen.com',
+    name: 'Hermione Granger',
+    email: 'hermione@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/4/4f/Daenerys_Season_8.jpg/revision/latest/scale-to-width-down/1000?cb=20190415212013',
+      'https://static.wikia.nocookie.net/harrypotter/images/3/34/Hermione_Granger.jpg/revision/latest/scale-to-width-down/1000?cb=20210522145306',
   },
   {
     id: '69643af1-9bb2-4e71-9171-932463ab6163',
-    name: 'Cersei Lannister',
-    email: 'cersei@lannister.com',
+    name: 'Ron Weasley',
+    email: 'ron@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/b/b0/CERSEIBATTLEOFKINGSLANDING.PNG/revision/latest?cb=20190906021534',
+      'https://static.wikia.nocookie.net/harrypotter/images/4/44/Ronald_Weasley_DHF1.jpg/revision/latest?cb=20101104210200',
   },
   {
     id: '4011adc0-0fef-4d8b-8ced-6a48ba372b94',
-    name: 'Tyrion Lannister',
-    email: 'tyrion@lannister.com',
+    name: 'Albus Dumbledore',
+    email: 'dumbledore@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG/revision/latest?cb=20190520175204',
+      'https://static.wikia.nocookie.net/harrypotter/images/7/78/Albus_Dumbledore_POA_promo.jpg/revision/latest?cb=20220801154717',
   },
   {
     id: '0d631020-b9a5-4ece-9c60-d9239d23b4f2',
-    name: 'Jaime Lannister',
-    email: 'jaime@lannister.com',
+    name: 'Severus Snape',
+    email: 'snape@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/5/51/JaimeLannisterSeason8.PNG/revision/latest?cb=20190207112813',
+      'https://static.wikia.nocookie.net/harrypotter/images/a/a3/Severus_Snape.jpg/revision/latest?cb=20150307193047',
   },
   {
     id: 'f3b1b7b1-5b7b-4b7b-8b7b-7b7b7b7b7b7b',
-    name: 'Robb Stark',
-    email: 'robb@stark.com',
+    name: 'Draco Malfoy',
+    email: 'draco@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/5/50/S3E9_Robb_Stark_main.jpg/revision/latest?cb=20160718071203',
+      'https://static.wikia.nocookie.net/harrypotter/images/7/7e/Draco_Malfoy_TDH.png/revision/latest/scale-to-width-down/1000?cb=20180116013508',
   },
   {
     id: 'a9b155c6-6f44-494f-84b5-2ef372bb3cdb',
-    name: 'Arya Stark',
-    email: 'arya@stark.com',
+    name: 'Rubeus Hagrid',
+    email: 'hagrid@hogwarts.com',
     avatar:
-      'https://static.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/revision/latest?cb=20190520174300',
+      'https://static.wikia.nocookie.net/harrypotter/images/f/fe/Rubeus_Hagrid.png/revision/latest?cb=20221014184333',
   },
 ];
